b282bc9aec1796b6a36418e2a790343ef04505a9
cmcolinh <colin.a.horner@gmail.com>
2017-03-16 12:59:26 +0900

    2017 Version of create.sql

--- create.sql
+++ create.sql
@@ -1,43 +1,69 @@
-CREATE TABLE tblplayer (
-  _key smallint(6) NOT NULL,
-  name varchar(40) DEFAULT NULL,
-  report int(11) DEFAULT NULL,
-  PRIMARY KEY (_key)
+CREATE TABLE tblcompetition (
+  _key smallint(6) NOT NULL AUTO_INCREMENT,
+  name varchar(100) DEFAULT NULL,
+  basepath varchar(100) DEFAULT NULL,
+  PRIMARY KEY (_key));
   
 CREATE TABLE tblgame (
-  _key smallint(6) NOT NULL,
+  _key smallint(6) NOT NULL AUTO_INCREMENT,
   gamename varchar(40) DEFAULT NULL,
-  ones int(11) DEFAULT NULL,
-  PRIMARY KEY (_key);
+  ones tinyint(4) DEFAULT NULL,
+  PRIMARY KEY (_key));
   
+CREATE TABLE tblscoringscheme (
+  _key smallint(6) NOT NULL AUTO_INCREMENT,
+  description varchar(40) DEFAULT NULL,
+  PRIMARY KEY (_key));
+
+CREATE TABLE tblmatch (
+  _key smallint(6) NOT NULL,
+  _fk_game smallint(6) DEFAULT NULL,
+  _fk_scoringscheme smallint(6) DEFAULT NULL,
+  PRIMARY KEY (_key),
+  FOREIGN KEY (_fk_game) REFERENCES tblgame (_key),
+  FOREIGN KEY (_fk_scoringscheme) REFERENCES tblscoringscheme (_key));          
+
+CREATE TABLE tblplayer (
+  _key smallint(6) NOT NULL AUTO_INCREMENT,
+  name varchar(40) DEFAULT NULL,
+  report int(11) DEFAULT NULL,
+  PRIMARY KEY (_key));
+  
+CREATE TABLE tblscoreset (
+  _key smallint(6) NOT NULL AUTO_INCREMENT,
+  _fk_player smallint(6) DEFAULT NULL,
+  _fk_competition smallint(6) DEFAULT NULL,
+  PRIMARY KEY (_key),
+  FOREIGN KEY (_fk_player) REFERENCES tblplayer (_key),
+  FOREIGN KEY (_fk_competition) REFERENCES tblcompetition (_key));
+ 
 CREATE TABLE tblscore (
-    _key smallint(6) NOT NULL,
-    _fk_player smallint(6) DEFAULT NULL,
-    _fk_game smallint(6) DEFAULT NULL,
-    score bigint(20) DEFAULT NULL,
-    points tinyint(4) DEFAULT NULL,
-    PRIMARY KEY (_key),
-    KEY _fk_player (_fk_player),
-    KEY _fk_game (_fk_game),
-    FOREIGN KEY (_fk_player) REFERENCES tblplayer(_key),
-   FOREIGN KEY (_fk_game) REFERENCES tblgame (_key);
-   
-   
-CREATE TABLE `tblmatch` (
-  `_key` smallint(6) NOT NULL,
-  `_fk_game` smallint(6) DEFAULT NULL,
-  `_fk_score_1` smallint(6) DEFAULT NULL,
-  `_fk_score_2` smallint(6) DEFAULT NULL,
-  `_fk_score_3` smallint(6) DEFAULT NULL,
-  `_fk_score_4` smallint(6) DEFAULT NULL,
-  PRIMARY KEY (`_key`),
-  KEY `_fk_game` (`_fk_game`),
-  KEY `_fk_score_1` (`_fk_score_1`),
-  KEY `_fk_score_2` (`_fk_score_2`),
-  KEY `_fk_score_3` (`_fk_score_3`),
-  KEY `_fk_score_4` (`_fk_score_4`),
-  FOREIGN KEY (`_fk_game`) REFERENCES tblgame(_key),
-  FOREIGN KEY (_fk_score_1) REFERENCES tblscore(_key),
-  FOREIGN KEY (_fk_score_2) REFERENCES tblscore(_key),
-  FOREIGN KEY (_fk_score_3) REFERENCES tblscore(_key),
-  FOREIGN KEY (_fk_score_4) REFERENCES tblscore(_key);
+  _key smallint(6) NOT NULL AUTO_INCREMENT,
+  _fk_player smallint(6) DEFAULT NULL,
+  _fk_match smallint(6) DEFAULT NULL,
+  _fk_scoreset smallint(6) DEFAULT NULL,
+  score bigint(20) DEFAULT NULL,
+  rank smallint(6) DEFAULT NULL,
+  points tinyint(4) DEFAULT NULL,
+  PRIMARY KEY (_key),
+  FOREIGN KEY (_fk_player) REFERENCES tblplayer (_key),
+  FOREIGN KEY (_fk_match) REFERENCES tblmatch (_key),
+  FOREIGN KEY (_fk_scoreset) REFERENCES tblscoreset (_key));
+  
+CREATE TABLE tblscoring (
+  _key smallint(6) NOT NULL AUTO_INCREMENT,
+  _fk_scoringscheme smallint(6) DEFAULT NULL,
+  numplayers smallint(6) DEFAULT NULL,
+  rank smallint(6) DEFAULT NULL,
+  pointsforrank smallint(6) DEFAULT NULL,
+  PRIMARY KEY (_key),
+  FOREIGN KEY (_fk_scoringscheme) REFERENCES tblscoringscheme (_key));
+     
+ 
+CREATE TABLE tblwebsitegenerator (
+  _key smallint(6) NOT NULL AUTO_INCREMENT,
+  _fk_competition smallint(6) DEFAULT NULL,
+  filename varchar(40) DEFAULT NULL,
+  filedef varchar(10000) DEFAULT NULL,
+  PRIMARY KEY (_key),
+  FOREIGN KEY (_fk_competition) REFERENCES tblcompetition (_key));
